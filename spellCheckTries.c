a8b75b98a0436f9d9266981adad3e61b6c6bc1d5
AkeemKaleeb <kaileb.j.cole@gmail.com>
2024-03-10 17:43:07 -0400

    Working Dictionary Implementation
    Implementation of a readable dictionary file, insertion into the Trie, and retrieval from the Trie

--- spellCheckTries.c
+++ spellCheckTries.c
@@ -21,122 +21,206 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#define NUM_CHAR 256        // Capital, Lowercase, space, new line
+#define NUM_CHAR 256            // Capital, Lowercase, space, new line
+#define BUFFER_SIZE 1024        // Read buffer to reduce SYS Calls
 
-typedef struct trienode {
+#define DICTFILE "words.txt"
+
+// #region TRIE
+
+typedef struct trienode
+{
     struct trienode *children[NUM_CHAR];
-    bool terminal;          // node is the end of a word
-}trieNode;
+    bool terminal; // node is the end of a word
+} dictNode;
 
 // function to create a new node in the trie
 // allocates space for the new node and the children to NULL values
-trieNode * createNode() {
-    trieNode *newNode = malloc(sizeof(*newNode));       // allocate space for a new node
+dictNode *createNode()
+{
+    dictNode *newNode = malloc(sizeof(*newNode)); // allocate space for a new node
 
-    for(int i = 0; i < NUM_CHAR; i++) {             
-        newNode->children[i] = NULL;                // initialize the new node to null characters
+    for (int i = 0; i < NUM_CHAR; i++)
+    {
+        newNode->children[i] = NULL; // initialize the new node to null characters
     }
 
-    newNode->terminal = false;                      // new node will not be terminal by default
-    return newNode;                                 // return the new node
+    newNode->terminal = false; // new node will not be terminal by default
+    return newNode;            // return the new node
 }
 
 // function to insert a new node into the trie
 // false implies the node already exists
 // true implies the node was inserted
-bool insertTrie(trieNode **root, char *signedText) {
-    if(*root == NULL) {
-        *root = createNode();                           // Create a new root if one doesn't exist
+bool insertTrie(dictNode **root, char *signedText)
+{
+    if (*root == NULL)
+    {
+        *root = createNode(); // Create a new root if one doesn't exist
     }
 
-    unsigned char *text = (unsigned char*)signedText;   // Prevents negative index in table
-    trieNode *tmp = *root;
+    unsigned char *text = (unsigned char *)signedText; // Prevents negative index in table
+    dictNode *tmp = *root;
     int length = strlen(signedText);
 
-    for(int i = 0; i < length; i++) {
-        if(tmp->children[text[i]] == NULL) {            // Create a new node if it does not exist
+    for (int i = 0; i < length; i++)
+    {
+        if (tmp->children[text[i]] == NULL)
+        { // Create a new node if it does not exist
             tmp->children[text[i]] = createNode();
         }
-        tmp = tmp->children[text[i]];                   // Moves the temp pointer to look at the appropriate child node
+        tmp = tmp->children[text[i]]; // Moves the temp pointer to look at the appropriate child node
     }
 
-    if(tmp->terminal) {
-        return false;           // checks if the word already exists
+    if (tmp->terminal)
+    {
+        return false; // checks if the word already exists
     }
-    else {
-        tmp->terminal = true;   // there was a node for this word, make it terminal
+    else
+    {
+        tmp->terminal = true; // there was a node for this word, make it terminal
         return true;
     }
 }
 
 // Recursive print function to print out the word in a trie
-void printTrie_Recursive(trieNode *node, unsigned char *prefix, int length) {
-    unsigned char newPrefix[length + 2];        // One more symbol and null character
-    memcpy(newPrefix, prefix, length);          // Copies the previous word to the new prefix with two extra spaces
-    newPrefix[length+1] = 0;                    // null terminate the string
+void printTrie_Recursive(dictNode *node, unsigned char *prefix, int length, int *wordNumber)
+{
+    unsigned char newPrefix[length + 2]; // One more symbol and null character
+    memcpy(newPrefix, prefix, length);   // Copies the previous word to the new prefix with two extra spaces
+    newPrefix[length + 1] = 0;           // null terminate the string
 
-    if(node->terminal) {                        // Base Case, word complete
-        printf("WORD: %s\n", prefix);
+    if (node->terminal)
+    { // Base Case, word complete
+        printf("WORD [%d]: %s\n", *wordNumber, prefix);
+        (*wordNumber)++;
     }
 
-    for(int i = 0; i < NUM_CHAR; i++) {                                         // Traverse all possible nodes
-        if(node->children[i] != NULL) {                                         // Check if a node has something worth checking
-            newPrefix[length] = i;                                              // Sets the value to be checked
-            printTrie_Recursive(node->children[i], newPrefix, length + 1);      // Recursively loop through the trie
+    for (int i = 0; i < NUM_CHAR; i++)
+    { // Traverse all possible nodes
+        if (node->children[i] != NULL)
+        {                                                                               // Check if a node has something worth checking
+            newPrefix[length] = i;                                                      // Sets the value to be checked
+            printTrie_Recursive(node->children[i], newPrefix, length + 1, wordNumber);  // Recursively loop through the trie
         }
     }
 }
 
 // Function to print out the trie in ASCII-betical order
-void printTrie(trieNode *root) {
-    if(root == NULL) {                  // Special case for when the TRIE is empty
+void printTrie(dictNode *root)
+{
+    if (root == NULL)
+    { // Special case for when the TRIE is empty
         printf("TRIE EMPTY\n");
         return;
     }
 
-    printTrie_Recursive(root, NULL, 0); // Call to recursive print function
+    int wordNumber = 1;     // Start with the first word
+    printTrie_Recursive(root, NULL, 0, &wordNumber); // Call to recursive print function
 }
 
 // Function to search if the word exists within the trie
 // True implies the word exists
 // False implies the word does not exist
-bool searchTrie(trieNode *root, char *signedText) {
-    unsigned char *text = (unsigned char*)signedText;       // Prevents negative indexes in the table
-    int length = strlen(signedText);                        // How long is the word we are searching for
-    trieNode *tmp = root;                                   // Temporary node, initialized to the root
+bool searchTrie(dictNode *root, char *signedText)
+{
+    unsigned char *text = (unsigned char *)signedText; // Prevents negative indexes in the table
+    int length = strlen(signedText);                   // How long is the word we are searching for
+    dictNode *tmp = root;                              // Temporary node, initialized to the root
 
-    for(int i = 0; i < length; i++) {                       // Search trie character by character
-        if(tmp->children[text[i]] == NULL) {                // Check if the ith character in the word exists
-            return false;                                   // Return false if the word does not exist
+    for (int i = 0; i < length; i++)
+    { // Search trie character by character
+        if (tmp->children[text[i]] == NULL)
+        {                 // Check if the ith character in the word exists
+            return false; // Return false if the word does not exist
         }
-        tmp = tmp->children[text[i]];                       // Search the next node based on the next character
+        tmp = tmp->children[text[i]]; // Search the next node based on the next character
     }
 
-    return tmp->terminal;                                   // Returns true only if the final node is terminal: the word exists and is not a substring of another word
+    return tmp->terminal; // Returns true only if the final node is terminal: the word exists and is not a substring of another word
 }
+
+// Function to recursively free the Trie
+void freeTrie(dictNode *node) {
+    if(node == NULL) {
+        return;
+    }
+
+    for(int i = 0; i < NUM_CHAR; i++) {
+        freeTrie(node->children[i]);
+    }
+
+    free(node);
+} 
+
+// #endregion
+
+// #region FILES
+
+// Function to fill the dictionary Trie
+// True implies dictionary was filled successfully
+// False implies dictionary was not filled successfully
+bool fillDictionary(const char* dictPath, dictNode **root) {
+    int dictionary_FD = open(dictPath, O_RDONLY);               // Open the dictionary in read only mode
+
+    if(dictionary_FD == -1) {                                   // Prints an error if the dictionary cannot be opened
+        perror("Dictionary could not be opened!\n");
+        return EXIT_FAILURE;
+    }
+
+    char buffer[BUFFER_SIZE];           // Buffer to reduce sys calls
+    int bytesRead;                      // number of bytes read so far
+    char word[BUFFER_SIZE];             // Buffer to store a single word
+    int wordLength = 0;                 // Length of the word stored
+
+    while((bytesRead = read(dictionary_FD, buffer, BUFFER_SIZE)) > 0) {     // Loop through the file
+        for(int i = 0; i < bytesRead; i++) {    // Loop through the word
+            if(buffer[i] == '\n') {             // End of word, insert to Trie
+                word[wordLength] = '\0';        // Terminate the word
+                if(wordLength > 0) {
+                    insertTrie(root, word);     // Insert complete word to Trie
+                    wordLength = 0;             // Reset word length for next word
+                }
+            }
+            else {
+                word[wordLength++] = buffer[i];     // Append the next character to the word
+            }
+        }
+    }
+
+    close(dictionary_FD);
+
+    return true;
+}
+
+// #endregion
 
 /***************************************************************************************************/
 /********************************THE ABOVE CODE WORKS AS INTENDED***********************************/
 /***************************************************************************************************/
 
 
+void testCases(dictNode *root) {
+    printf("search for Cattle: %d\n", searchTrie(root, "Cattle"));
+    printf("search for Link: %d\n", searchTrie(root, "Link"));
+    printf("search for Apple: %d\n", searchTrie(root, "Apple"));
+    printf("search for apple: %d\n", searchTrie(root, "apple"));
+    printf("search for efpa: %d\n", searchTrie(root, "efpa"));
+}
 
-int main() {
-    trieNode *root = NULL;
-    insertTrie(&root, "KIT");
-    insertTrie(&root, "CATTLE");
-    insertTrie(&root, "KIN");
-    insertTrie(&root, "CAT");
-    insertTrie(&root, "HAPPY");
+int main()
+{
+    dictNode *root = NULL;
 
+    fillDictionary(DICTFILE, &root);
     printTrie(root);
+    //testCases(root);
 
-    printf("search for CATTLE: %d\n", searchTrie(root, "CATTLE"));
-    printf("search for LINK: %d\n", searchTrie(root, "LINK"));
-    printf("search for HAPPY: %d\n", searchTrie(root, "HAPPY"));
-    printf("search for CAT: %d\n", searchTrie(root, "CAT"));
-    printf("search for KITTEN: %d\n", searchTrie(root, "KITTEN"));
-    
+
+    freeTrie(root);
+
     return EXIT_SUCCESS;
 }
