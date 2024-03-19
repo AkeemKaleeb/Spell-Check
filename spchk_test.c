912ce02ae6c1e1482f63d814bd92f35abb7a0f19
shreyash618 <115910588+shreyash618@users.noreply.github.com>
2024-03-18 23:00:34 -0400

    Update spchk_test.c

--- spchk_test.c
+++ spchk_test.c
@@ -20,32 +20,29 @@ int main (int argc, char * argv[]){
     test (1);       
 
     /*********TEST 2:*********/
-    //check what happens when there are no text files in the specified directory
-    //expected output: nothing should be printed out
-    test (2);       //no dict file
+    //we should test a file with a lot of punctuation (!@#$%^&*(){})
+    //expected output: the words should not be reported as incorrect, even with the punctuation
+    test (2);       //normal punctuation    //jane eyre file
 
     /*********TEST 3:*********/
     //we should test a file with a lot of punctuation (!@#$%^&*(){})
     //expected output: the words should not be reported as incorrect, even with the punctuation
-    test (3);       //normal punctuation    //jane eyre file
+    test (3);       //abnormal punctuation
 
     /*********TEST 4:*********/
-    //we should test a file with a lot of punctuation (!@#$%^&*(){})
-    //expected output: the words should not be reported as incorrect, even with the punctuation
-    test (4);       //abnormal punctuation
-
-    /*********TEST 5:*********/
     //we should use one of the text files with incorrect punctuation
-    //test (5);       //incorrectly spelled words
+    test (4);       //incorrectly spelled words
 
-    /*********TEST 6:*********/
-    //we should use one of the text files (with incorrectly spelled words) as it's own dictionary file
-    //expected output: the program should run smoothly without any errors
-    //test (6);
+    /*********Test 5:*********/
+    //we should test all the issues relating to capitilzation, punctuation, spelling, etc.
+    //they should all be reported
 
-    /*********Test 7:*********/
+    test (5);
+
+    /*********Test 6:*********/
     //we should use a file containing words with incorrect capitilzation (ie, MacDonalds or MACDONALDS should work, but not macdonalds)
     //they should all be reported
-    //test (7);
+    test (6);
+
     return 0;
 }
